33192facfbf08772c7b1e5e9a61cd209f928e617
unknown <alejandro-hdez115@outlook.com>
2018-02-10 13:03:34 -0600

    Article box samples
    Added article box samples, with a specific class to be used by the first article.

--- source/index.php
+++ source/index.php
@@ -6,17 +6,42 @@
     <title>Inicio | Aldan Project</title>
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="lib/styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Montserrat" rel="stylesheet">
   </head>
-  <body>
+  <body class="blog">
     <h1>Aldan Project</h1>
     <!-- Navigation bar -->
-    <ul>
+    <ul class="main-menu">
       <li><a href="#">Inicio</a></li>
       <li><a href="#">Descargas</a></li>
       <li><a href="#">Foros</a></li>
       <li><a href="#">Acerca de Aldan Project</a></li>
     </ul>
     <!-- Navigation bar -->
+
+    <!-- Articles section -->
+    <article class="simple-article first-article" id="blog01">
+      <img src="img/blog01.jpg">
+      <h2>Título del primer artículo</h2>
+      <p>
+        Este es el <b>primer</b> articulo en la página de <b>Aldan Project</b>, aquí puedes insertar información relacionada sobre el tema que se habla
+        en el articulo.<br><br>
+        Incluyendo una <b>imágen</b> que lo acompaña a su lado izquierdo.<br><br>
+        Y aquí hay un <a href="">enlace</a>. Contiene una clase específica para el primer articulo mostrado.
+      </p>
+      <p class="read-more"><a href="">Leer más</a></p>
+    </article>
+    <article class="simple-article" id="blog02">
+      <img src="img/blog02.jpg">
+      <h2>Título del segundo artículo</h2>
+      <p>
+        Este es el <b>segundo</b> articulo en la página de <b>Aldan Project</b>, aquí puedes insertar información relacionada sobre el tema que se habla
+        en el articulo.<br><br>
+        Incluyendo una <b>imágen</b> que lo acompaña a su lado izquierdo.<br><br>
+        Y aquí hay un <a href="">enlace</a>. Contiene una clase específica para el primer articulo mostrado.
+      </p>
+      <p class="read-more"><a href="">Leer más</a></p>
+    </article>
+    <!-- Articles section -->
   </body>
 </html>
