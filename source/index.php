655a441f40fd99ad244d50a0f4bae5956788a133
unknown <alejandro-hdez115@outlook.com>
2018-02-09 21:27:06 -0600

    Initial page style
    Added files:
    * lib/styles.css
    
    Now index have navigation bar.

--- source/index.php
+++ source/index.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
+<!-- Made by Aldan Project -->
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Inicio</title>
+    <title>Inicio | Aldan Project</title>
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="lib/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
   </head>
   <body>
-
+    <h1>Aldan Project</h1>
+    <!-- Navigation bar -->
+    <ul>
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Descargas</a></li>
+      <li><a href="#">Foros</a></li>
+      <li><a href="#">Acerca de Aldan Project</a></li>
+    </ul>
+    <!-- Navigation bar -->
   </body>
 </html>
