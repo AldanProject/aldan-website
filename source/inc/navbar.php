4771bae1650a35b68f7c8d9442d75d6bbd264a97
unknown <alejandro-hdez115@outlook.com>
2018-02-10 22:48:34 -0600

    Includes, footer and size
    Footer was added.
    Navbar, articles and footer are called by "include" from PHP.
    Design:
    The web page size was changed from 65% to 75% and some margins were modified.

---
+++ source/inc/navbar.php
@@ -0,0 +1,6 @@
+<ul class="main-menu">
+  <li><a href="#">Inicio</a></li>
+  <li><a href="#">Descargas</a></li>
+  <li><a href="#">Foros</a></li>
+  <li><a href="#">Acerca de Aldan Project</a></li>
+</ul>
--- source/index.php
+++ source/index.php
@@ -1,47 +1,25 @@
 <!DOCTYPE html>
-<!-- Made by Aldan Project -->
+<!-- Made by Aldan Project | 2018 -->
 <html>
   <head>
     <meta charset="utf-8">
     <title>Inicio | Aldan Project</title>
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="lib/styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Montserrat|Poppins" rel="stylesheet">
   </head>
   <body class="blog">
     <h1>Aldan Project</h1>
     <!-- Navigation bar -->
-    <ul class="main-menu">
-      <li><a href="#">Inicio</a></li>
-      <li><a href="#">Descargas</a></li>
-      <li><a href="#">Foros</a></li>
-      <li><a href="#">Acerca de Aldan Project</a></li>
-    </ul>
+    <?php include("inc/navbar.php"); ?>
     <!-- Navigation bar -->
 
     <!-- Articles section -->
-    <article class="simple-article first-article" id="blog01">
-      <img src="img/blog01.jpg">
-      <h2>Título del primer artículo</h2>
-      <p>
-        Este es el <b>primer</b> articulo en la página de <b>Aldan Project</b>, aquí puedes insertar información relacionada sobre el tema que se habla
-        en el articulo.<br><br>
-        Incluyendo una <b>imágen</b> que lo acompaña a su lado izquierdo.<br><br>
-        Y aquí hay un <a href="">enlace</a>. Contiene una clase específica para el primer articulo mostrado.
-      </p>
-      <p class="read-more"><a href="">Leer más</a></p>
-    </article>
-    <article class="simple-article" id="blog02">
-      <img src="img/blog02.jpg">
-      <h2>Título del segundo artículo</h2>
-      <p>
-        Este es el <b>segundo</b> articulo en la página de <b>Aldan Project</b>, aquí puedes insertar información relacionada sobre el tema que se habla
-        en el articulo.<br><br>
-        Incluyendo una <b>imágen</b> que lo acompaña a su lado izquierdo.<br><br>
-        Y aquí hay un <a href="">enlace</a>. Contiene una clase específica para el primer articulo mostrado.
-      </p>
-      <p class="read-more"><a href="">Leer más</a></p>
-    </article>
+    <?php include("inc/articles.php"); ?>
     <!-- Articles section -->
+
+    <!-- Footer -->
+    <?php include("inc/footer.php"); ?>
+    <!-- Footer -->
   </body>
 </html>
